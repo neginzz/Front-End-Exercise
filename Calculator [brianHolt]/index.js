ed9feba3078f004681b9042420309012b8aec1ec
neginz <negin.z566@gmail.com>
2024-01-14 21:59:41 +0330

    almost finish js

--- Calculator [brianHolt]/index.js
+++ Calculator [brianHolt]/index.js
@@ -1,53 +1,79 @@
-
-const forTextbox =document.querySelectorAll(".btn-num, .btn-opt:not(.btn-eq)");
-const allBtn=document.querySelectorAll(".btn");
-// const txtSet=document.querySelector(".txt-input");
+let buffer = "0";
+let total = 0;
+let prevopt = null;
+const forTextbox = document.querySelectorAll(".btn-num, .btn-opt:not(.btn-eq)");
+const allBtn = document.querySelectorAll(".btn");
+const txtSet = document.querySelector(".txt-input");
 // const operation= document.querySelectorAll(".btn-opt");
 
-for(const item of forTextbox){
- item.addEventListener("click", function (event) {
-  document.querySelector(".txt-input").value = event.target.innerHTML;});
-};
-
-for(const item of allBtn){
+for (const item of allBtn) {
   item.addEventListener("click", function (event) {
-    if(event.target.classList.contains('btn-num')){
-      console.log('number:', event.target.innerHTML);
+    let inside = event.target.innerHTML;
+
+    if (event.target.classList.contains("btn-num")) {
+      // console.log("number:", inside);
+
+      buffer = inside;
+      if (txtSet.value === "0") {
+        txtSet.value = buffer;
+      } else {
+        txtSet.value += buffer;
+      }
+    } else if (event.target.classList.contains("btn-c")) {
+      buffer = 0;
+      txtSet.value = buffer;
+    } else if (event.target.classList.contains("btn-del")) {
+      if (txtSet.value.length === 1) {
+        txtSet.value = "0";
+      } else {
+        txtSet.value = txtSet.value.substring(0, txtSet.value.length - 1);
+      }
+    } else if (event.target.classList.contains("btn-eq")) {
+      if (prevopt === null) {
+        return;
+      } else {
+        compute(parseInt(txtSet.value));
+        txtSet.value = "" + total;
+        total = 0;
+      }
+    } else {
+      // console.log(txtSet.value, inside);
+      calculate(txtSet.value, inside);
     }
-    else{
-      console.log('symbol',event.target.innerHTML);
-    }
- })};
-
-
-// 
- 
-
- 
-
-
-
-
-
-function calculate(num1, num2, opt) {
-  let result = 0;
-  switch (opt) {
-    case "÷":
-      result = num1 / num2;
-      break;
-    case "x":
-      result = num1 * num2;
-      break;
-    case "-":
-      result = num1 - num2;
-      break;
-    case "+":
-      result = num1 + num2;
-      break;
-    default:
-        console.log('not valid');
-  }
-  return result;
+  });
 }
 
-// console.log(calculate(1,2,'+'));
+function calculate(num, opt) {
+  if (num === "0") {
+    return;
+  }
+  const intNum = parseInt(num);
+  if (total === 0) {
+    total = intNum;
+  } else {
+    compute(intNum);
+  }
+
+  prevopt = opt;
+  txtSet.value = "0";
+  // console.log(total);
+}
+
+function compute(num) {
+  switch (prevopt) {
+    case "÷":
+      total /= num;
+      break;
+    case "x":
+      total *= num;
+      break;
+    case "-":
+      total -= num;
+      break;
+    case "+":
+      total += num;
+      break;
+    default:
+      console.log("not valid");
+  }
+}
