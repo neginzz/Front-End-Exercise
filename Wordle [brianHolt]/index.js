15217fbf06fe140e349dfb29a6038f3508af69b1
neginz <negin.z566@gmail.com>
2024-01-21 14:11:31 +0330

    all done w js

--- Wordle [brianHolt]/index.js
+++ Wordle [brianHolt]/index.js
@@ -3,21 +3,27 @@
 const letters = document.querySelectorAll(".score-letter");
 const loadingDiv = document.querySelector(".info-bar");
 const ANSWER_LENGHT = 5;
+const ROUNDS = 6;
 
 // work on typing
 
 async function init() {
   let currentGuess = "";
   let currenRow = 0;
+  let isLoading = true;
 
   // U only req the word of the day once,(when the page is loaded)
   //res= coomon shorthand for response
+  // if u want a new word each time
+  // const res = await fetch("https://words.dev-apis.com/word-of-the-day?random=1");
   const res = await fetch("https://words.dev-apis.com/word-of-the-day");
   const resObj = await res.json();
   const word = resObj.word.toUpperCase();
   const wordParts = word.split("");
   // console.log(word);
+  let done = false;
   setLoading(false);
+  isLoading = false;
 
   function addLetter(letter) {
     if (currentGuess.length < ANSWER_LENGHT) {
@@ -38,12 +44,30 @@ async function init() {
       return;
     }
 
-    // TODO validate the word
+    // validate the word
+    isLoading = true;
+    setLoading(isLoading);
+    const res = await fetch("https://words.dev-apis.com/validate-word", {
+      method: "POST",
+      body: JSON.stringify({ word: currentGuess }),
+    });
 
-    // TODO do all the marking : "wrong" "close" "right"
+    const resObj = await res.json();
+    const validWord = resObj.validWord;
+    // const {validWord} = resObj;
+
+    isLoading = false;
+    setLoading(isLoading);
+
+    if (!validWord) {
+      markInValinWord();
+      return;
+    }
+
+    //do all the marking : "wrong" "close" "right"
     const guessParts = currentGuess.split("");
     const map = makeMap(wordParts);
-    console.log(map);
+    // console.log(map);
 
     for (let i = 0; i < ANSWER_LENGHT; i++) {
       //Mark as correct
@@ -65,9 +89,19 @@ async function init() {
       }
     }
 
-    // TODO did they win or lose
-
     currenRow++;
+
+    // TODO did they win or lose
+    if (currentGuess === word) {
+      //win
+      alert("U win!");
+      document.querySelector(".title").classList.add("winner");
+      done = true;
+      return;
+    } else if (currenRow === ROUNDS) {
+      alert(`U loose the word was ${word}`);
+      done = true;
+    }
     currentGuess = "";
   }
 
@@ -75,10 +109,27 @@ async function init() {
     currentGuess = currentGuess.substring(0, currentGuess.length - 1);
     letters[ANSWER_LENGHT * currenRow + currentGuess.length].innerText = "";
   }
+  function markInValinWord() {
+    // alert("not a valid word");
+
+    for (let i = 0; i < ANSWER_LENGHT; i++) {
+      letters[ANSWER_LENGHT * currenRow + i].classList.remove("invalid");
+
+      // how long we want to keep the invalid class
+      setTimeout(function () {
+        letters[ANSWER_LENGHT * currenRow + i].classList.add("invalid", 10);
+      });
+    }
+  }
 
   document.addEventListener("keydown", function handleKeyPress(event) {
     const action = event.key;
     // console.log(action);
+
+    if (done || isLoading) {
+      // do nothing
+      return;
+    }
 
     if (action === "Enter") {
       commit();
